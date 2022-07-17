07b56ac697d135f62af3febf346d3c8c3d882ae2
Kristian Dupont <kristian@kristiandupont.com>
2022-07-17 00:37:30 +0000

    Add tryParse

--- index.d.ts
+++ index.d.ts
@@ -9,3 +9,17 @@ export declare const parse: (source: string) => {
     [index: string]: boolean | string | string[];
   };
 };
+
+/**
+ * Parse a possibly tagged string.
+ * If the string has a syntax error according to the grammar,
+ * no error is thrown. The string is returned as the comment part of the result.
+ * Example: "This is a comment that has tags @a @b:123"
+ * returns: { comment: "This is a comment that has tags", tags: { a: true, b: '123' }}
+ */
+export declare const tryParse: (source: string) => {
+  comment?: string | undefined;
+  tags?: {
+    [index: string]: boolean | string | string[];
+  };
+};
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const { parse, SyntaxError } = require("./parser");
 
+const tryParse = (str) => {
+  try {
+    return parse(str);
+  } catch (e) {
+    return { comment: str || undefined, tags: {} };
+  }
+};
+
 module.exports = {
   parse,
+  tryParse,
   SyntaxError,
 };
